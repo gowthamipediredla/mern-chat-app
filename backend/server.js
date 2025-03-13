cc4ed3e9fb31e21766bf4f6a22631653d72a3744
GowthamiPediredla <Gowthami.Pediredla@academy.com>
2025-03-13 17:14:25 +0530

    mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const { connectDB } = require("./config/db");
+const colors = require("colors");
 const app = express();
 app.use(cors());
 
 dotenv.config();
-
+connectDB();
 app.get("/", (req, res) => {
   res.send("api running");
 });
@@ -21,4 +23,4 @@ app.get("/api/chat/:id", (req, res) => {
   res.send(filteredUser);
 });
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log("server started on port " + PORT));
+app.listen(PORT, console.log("server started on port " + PORT.yellow.bold));
